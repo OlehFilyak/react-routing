1ce5e76574cb90c9b4c040017c7184a1c545e3ab
Олег Філяк <73895897+OlehFilyak@users.noreply.github.com>
2021-07-26 17:44:24 +0300

    copied starting project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
-import "./App.css";
+import React, {Component} from 'react'
+import './App.scss'
+import {Route} from 'react-router-dom'
+import About from './About/About'
+import Cars from './Cars/Cars'
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  render() {
+
+    return (
+      <div>
+        <nav className="nav">
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+          </ul>
+        </nav>
+
+        <hr/>
+
+        {/*localhost:3000*/}
+        <Route path="/" exact render={() => <h1>Home Page</h1>} />
+
+        <About />
+
+        <Cars />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default App
